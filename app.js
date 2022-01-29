fdd391f0bf057df0880f03ef50eaf089cbc99da6
bilalE99 <bilalelfitouri1999@gmail.com>
2022-01-29 21:49:53 +0000

    mysql database created and connection made.

--- app.js
+++ app.js
@@ -21,6 +21,22 @@ app.use(express.static('public'));
 app.engine('hbs', exphbs.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
+
+//Connection Pool
+const pool = mysql.createPool({
+    connectionLimit : 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+//Connec to DB
+pool.getConnection((err, connection) => {
+    if(err) throw err;
+    console.log('Connection ID ' + connection.threadId)
+});
+
 //Router
 app.get('', (req, res) => {
     res.render('home');
